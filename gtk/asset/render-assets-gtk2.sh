2659f85201b33526b5bd5e3920714543bdb19c6d
tista500 <tista.gma500@gmail.com>
2016-06-12 02:43:54 +0900

    Adapta: Add GPLv2 header to shell-scripts

--- gtk/asset/render-assets-gtk2.sh
+++ gtk/asset/render-assets-gtk2.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 #
-# Yeah this script is pretty bad and ugly, so?
+# This file is part of adapta-gtk-theme
+#
+# Copyright (C) 2016 Tista <tista.gma500@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
 
 INKSCAPE="/usr/bin/inkscape"
