3d98bed7b108fb9c084c4d7ffb15613900fc4267
tista500 <tista.gma500@gmail.com>
2016-06-16 01:37:47 +0900

    Makefile.am: [Wm] Add color-changer for Metacity/XFwm4

--- wm/asset/clone-assets-metacity.sh
+++ wm/asset/clone-assets-metacity.sh
@@ -13,6 +13,27 @@
 SRC_DIR="assets-metacity"
 ASSETS_DIR="../metacity-1"
 INDEX="assets-metacity.txt"
+RECOLOR_FILE="./assets-metacity/button_close_pressed.svg"
+KEY_FILE="../../gtk/sass/common/_key_colors.scss"
+
+# Default colours
+selection1="`grep 'Cyan500' ../../gtk/sass/common/_colors.scss | \
+                    cut -d' ' -f3`"
+
+# Check and re-color 'button_close_pressed' button
+if [ -e $KEY_FILE ]; then
+    selection2="`grep 'key_selection' $KEY_FILE | \
+                 cut -d' ' -f2 | cut -d';' -f1`"
+
+    cp -f $RECOLOR_FILE.in $RECOLOR_FILE
+    if [ $selection1 != $selection2 ]; then
+        sed -i "s/$selection1/$selection2/g" $RECOLOR_FILE
+        echo $selection1 is re-colored with $selection2.
+    fi
+else
+    echo _key_colors.scss was not found. Stopped...
+    exit 1
+fi
 
 # Clone stock SVG files
 for i in `cat $INDEX`
